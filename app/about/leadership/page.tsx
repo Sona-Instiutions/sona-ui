de8236b25c363f1c90200ce7d4c99d2ec90a0132
Smitha-Ganeshappa <smithag89@gmail.com>
2025-11-13 14:25:11 +0530

    about us leadership page work in progress

---
+++ app/about/leadership/page.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import CallToActionSection from "@/components/common/CallToActionSection";
+
+
+
+export const metadata: Metadata = {
+  title: "SONA Institutions | Leadership",
+  description:
+    "Welcome to SONA Institutions. Explore our programs, placements, and world-class facilities for engineering, technology, and management education.",
+  openGraph: {
+    title: "SONA Institutions | Leadership",
+    description:
+      "Discover SONA Institutions – excellence in education, innovation, and industry collaboration.",
+    type: "website",
+    url: "https://sona.edu.in/",
+    siteName: "SONA Institutions",
+    images: [
+      {
+        url: "/images/home-banner.jpg",
+        width: 1200,
+        height: 630,
+        alt: "SONA Institutions Campus",
+      },
+    ],
+  },
+};
+
+export default function HomePage() {
+  
+  return (
+      <div className="min-h-screen bg-white">
+    {/* BreadcrumbBanner Section  */}
+    
+     
+      
+      {/* CallToActionSection section  */}
+
+      <CallToActionSection
+        bgColor="bg-yellow-500"
+        heading="Ready to Shape Your Future?"
+        description="Join thousands of successful graduates who chose SCALE to transform their dreams into reality. Your journey to excellence starts here."
+        
+      />
+
+    </div>
+  );
+}
