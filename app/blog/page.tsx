d4f46a4844b782963335be3bef8966ea0e401ce4
Smitha-Ganeshappa <smithag89@gmail.com>
2025-12-22 21:19:46 +0530

    fixing the build errors

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import MainBanner from "@/components/common/MainBannerSection";
-import BlogListing from "@/components/blog/BlogListing";
+import BlogListing from "@/components/blog/BlogList";
 
 
 export const metadata: Metadata = {
